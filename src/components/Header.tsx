4f84dc84050842cbd0e7042babc8c35f6a84b583
Lorenzo Dalla Muta <lorenzodallamuta@gmail.com>
2025-08-28 18:22:11 +0200

    Header, add spacing between right side ctas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,7 +44,7 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
           </>
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
+            <Button size="small" onClick={onLogin} label="Log in" />{" "}
             <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
           </>
         )}
