2356caa00f53c919f30a2016d239f184a9e34dc3
Lorenzo Dalla Muta <lorenzodallamuta@gmail.com>
2025-08-29 15:30:24 +0200

    Card, the avatar src string should be an external prop as well, better testability, better approx. real-world usage

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,19 @@
 import { Box, Text } from '@radix-ui/themes';
-import { faker } from '@faker-js/faker';
 import type { ComponentProps } from 'react';
 import { StyledAvatar, StyledCard } from './card.styled';
 
 export interface CardProps extends ComponentProps<typeof StyledCard> {
   userName: string,
   summary: string,
+  avatar: string,
   maxWidth?: string
 }
 
-export const Card = ({ maxWidth="350px", summary, userName, ...props }: CardProps) => (
+export const Card = ({ maxWidth, summary, userName, avatar, ...props }: CardProps) => (
   <Box maxWidth={maxWidth}>
 	<StyledCard asChild {...props}>
 		<a href="#">
-			<StyledAvatar src={faker.image.avatar()}/>
+			<StyledAvatar src={avatar}/>
 			<Text as="div" size="2" weight="bold">
 				{userName}
 			</Text>
--- src/stories/cards.mock.tsx
+++ src/stories/cards.mock.tsx
@@ -4,6 +4,7 @@ import type { CardProps } from "../components/Card";
 const cardMock = (): CardProps => ({
   userName: faker.person.fullName(),
   summary: faker.person.bio(),
+  avatar: faker.image.avatar()
 });
 
 export const cardsMock = (number = 9) => Array.from({ length: number }, () => cardMock());
