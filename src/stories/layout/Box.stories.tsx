25ee85a77b748dcd74f33e3d5d233dcb1187d25f
Lorenzo Dalla Muta <lorenzodallamuta@gmail.com>
2025-09-01 15:49:10 +0200

    Box, implement story for Box (only basic orios controls)

---
+++ src/stories/layout/Box.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { fn } from 'storybook/test';
+
+import { Box } from '../../atoms/layout/Box';
+
+const meta = {
+  title: 'Layout/Box',
+  component: Box,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    as: { control: 'radio', options: ['div', 'span', undefined], if: { arg: 'asChild', truthy: false }, table: {defaultValue: {summary: 'div'}} },
+    asChild: { control: 'boolean', type: 'boolean', if: { arg: 'as', truthy: false }, table: {defaultValue: {summary: 'false'}} },
+    display: { control: 'inline-radio', options: ['none', 'inline', 'inline-block', 'block', 'contents'], table: { defaultValue: { summary: 'block' } } },
+  },
+  render: ({ children, ...props }) => (<div style={{backgroundColor: '#ffffc0'}}><Box {...props}>{children ?? 'Box'}</Box></div>),
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() },
+} satisfies Meta<typeof Box>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
